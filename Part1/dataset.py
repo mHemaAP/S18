605b2a01ea23487c048938c0a2863510585553c2
mHemaAP <135347943+mHemaAP@users.noreply.github.com>
2023-10-29 21:42:03 +0530

    Lightning code

--- Part1/dataset.py
+++ Part1/dataset.py
@@ -1,91 +1,57 @@
-import os
 import torch
 import torchvision
-from torchvision import transforms
-import torchvision.transforms as T
-from torch.utils.data import Dataset, DataLoader
-import multiprocessing
+import pytorch_lightning as pl
+    
+from torchvision import transforms    
+    
+class OxfordIIITPetsData(pl.LightningDataModule):
+    def __init__(self, train_data_path='data\\OxfordPets\\train', 
+                 test_data_path='data\\OxfordPets\\test', batch_size=16):
+        super().__init__()       
+        self.train_data_path = train_data_path
+        self.test_data_path = test_data_path
+        self.batch_size = batch_size
 
-IMG_SIZE    = 128
-BATCH_SIZE  = 16
-NUM_WORKERS = int(multiprocessing.cpu_count() / 2)
+    def prepare_data(self):
+        # Create a torchvision transform to resize the image
+        target_size = (400,600)
+        transform_resize_tensor = transforms.Compose([
+        transforms.Resize(target_size),
+        # This converts the PIL Image to a PyTorch tensor
+        transforms.ToTensor()  
+        ])
 
-def get_dataset(dir):
-  # Oxford IIIT Pets Segmentation dataset loaded via torchvision.
-  train_data_path = os.path.join(dir, 'OxfordPets', 'train')
-  test_data_path = os.path.join(dir, 'OxfordPets', 'test')
-  train_dataset = torchvision.datasets.OxfordIIITPet(root=train_data_path, split="trainval", target_types="segmentation", download=True)
-  test_dataset = torchvision.datasets.OxfordIIITPet(root=test_data_path, split="test", target_types="segmentation", download=True)
-  return train_data_path, train_dataset, test_data_path, test_dataset
+        # Oxford IIIT Pets Segmentation dataset loaded via torchvision.       
+        self.train_data = torchvision.datasets.OxfordIIITPet(root=self.train_data_path, split="trainval", 
+                                                             target_types="segmentation", download=True, 
+                                                             transform=transform_resize_tensor, 
+                                                             target_transform=transform_resize_tensor)
+        self.test_data = torchvision.datasets.OxfordIIITPet(root=self.test_data_path, split="test", 
+                                                            target_types="segmentation", download=True, 
+                                                            transform=transform_resize_tensor, 
+                                                            target_transform = transform_resize_tensor)
 
-#train_data_path, train_dataset, test_data_path, test_dataset = get_dataset(dir="./data")
+    def setup(self, stage):
+        pass
+                 
 
-class OxfordDataset(torchvision.datasets.OxfordIIITPet):
-  def __init__(self,
-               root: str,
-        split: str,
-        target_types="segmentation",
-        download=False,transform=None):
-
-    super().__init__(
-            root=root,
-            split=split,
-            target_types=target_types,
-            download=download,
-            transform=transform,
-        )
-    self.transform = transform
-
-  def __len__(self):
-    return super().__len__()
-
-  def __getitem__(self,index):
-    (img, mask_img) = super().__getitem__(index) # img is already a tensor
-    mask_img = self.transform(mask_img)
-    mask_img = mask_img * 255
-    mask_img = mask_img.to(torch.long)
-    mask_img = mask_img - 1
-    return (img, mask_img)
-
-def get_common_transform():
-  common_transform = transforms.Compose([
-      transforms.Resize((IMG_SIZE,IMG_SIZE), interpolation=T.InterpolationMode.NEAREST),
-      transforms.ToTensor(),  # normalizes by default and converts to tensor
-  ])
-  return common_transform
-
-
-def get_train_data_transform(root):
-  transformed_train_data = OxfordDataset(root=root,
-                                         split="trainval",
-                                         target_types="segmentation",
-                                         download=False, transform = get_common_transform())
-  return transformed_train_data
-
-
-def get_test_data_transform(root):
-
-  transformed_test_data = OxfordDataset(root=root,
-                                        split="test",
-                                        target_types="segmentation",
-                                        download=False,
-                                        transform = get_common_transform(),
-                                       )
-  return transformed_test_data
-
-
-def get_dataloaders(train_data_path, test_data_path):
-
-  transformed_train_data = get_train_data_transform(train_data_path)
-  transformed_test_data = get_test_data_transform(test_data_path)
-
-  # DATALOADERS
-  train_dataloader = DataLoader(transformed_train_data, batch_size = BATCH_SIZE,
-                                shuffle=True, num_workers=NUM_WORKERS,
-                                pin_memory=True)
-  test_dataloader = DataLoader(transformed_test_data, batch_size = BATCH_SIZE,
-                                shuffle=False, num_workers=NUM_WORKERS,
-                                pin_memory=True)
-  return train_dataloader, test_dataloader
-
-
+    def train_dataloader(self):
+        return  torch.utils.data.DataLoader(
+            self.train_data,
+            batch_size= self.batch_size,
+            shuffle=True,
+            )
+    
+    def test_dataloader(self):
+         return torch.utils.data.DataLoader(
+            self.test_data,
+            batch_size= self.batch_size,
+            shuffle=False,
+            )
+    
+    def val_dataloader(self):
+         return torch.utils.data.DataLoader(
+            self.test_data,
+            batch_size=self.batch_size,
+            shuffle=True,
+            )
--- Part1/utils.py
+++ Part1/utils.py
@@ -3,7 +3,35 @@ import matplotlib.pyplot as plt
 import torchvision.transforms as T
 import torch
 import torch.nn as nn
+from torch.nn import functional as F
 import random
+import numpy as np
+
+
+class DiceLoss(nn.Module):
+    def __init__(self):
+        super(DiceLoss, self).__init__()
+
+    def forward(self, pred, target):
+        smooth = 1e-5
+
+        # Reshape the predicted output tensor to [batch_size * height * width, num_classes]
+        pred = pred.permute(0, 2, 3, 1).contiguous().view(-1, 3)
+        pred = F.softmax(pred,dim=1)
+        # Reshape the tensor back to [batch_size, 3, height, width]
+        pred = pred.view(-1, 400, 600, 3).permute(0, 3, 1, 2)
+
+        target = torch.cat([ (target == i) for i in range(1,4) ], dim=1)
+
+        target = target.view(-1)
+        pred = pred.reshape(-1)
+        intersection = (pred * target).sum()
+        union = pred.sum() + target.sum()
+
+       
+        dice = (2. * intersection + smooth) / (union + smooth)
+      
+        return torch.mean(1 - dice)
 
 def show_sample_images(train_dataset):
 
@@ -26,24 +54,35 @@ def plot_losses(train_losses, test_losses, title):
   fig.suptitle(title, fontsize=16, x=0.5, y=1.10)
 
 
-def show_image_results(model, test_dataloader):
-    model.eval()
-    model.to('cpu')
-    transform = T.ToPILImage()
-    (test_inputs, test_targets) = next(iter(test_dataloader))
-    fig, arr = plt.subplots(6, 3, figsize=(15, 15)) # batch size 16
+def visualize_image_results(test_dataloader, litmodel, batch_size):
+
+    #test_dataloader = datamodule.test_dataloader()
+    for batch in test_dataloader:
+        images, masks = batch
+        # Break to get the first batch (a batch of images and labels)
+        break  
 
     for index in range(6):
-      img = test_inputs[index].unsqueeze(0)
-      pred_y = model(img)
-      pred_y = nn.Softmax(dim=1)(pred_y)
-      pred_mask = pred_y.argmax(dim=1)
-      pred_mask = pred_mask.unsqueeze(1).to(torch.float)
+        # Select one image and its corresponding label
+        index = random.randint(0, (batch_size-1))
+        image = images[index]
+        mask = masks[index]*255
 
+        with torch.no_grad():
+            input_image = image.to("cpu").unsqueeze(0)
+            pred_mask_prob = litmodel.model(input_image)
 
-      arr[index,0].imshow(transform(test_inputs[index]))
-      arr[index,0].set_title('Processed Image')
-      arr[index,1].imshow(transform(test_targets[index].float()))
-      arr[index,1].set_title('Actual Masked Image ')
-      arr[index,2].imshow(pred_mask.squeeze(0).squeeze(0))
-      arr[index,2].set_title('Predicted Masked Image ')
+        # From the probability of predictions for 3 classes, 
+        # construct the actual prediction of values - 1,2,3
+        pred_mask = torch.argmax(pred_mask_prob, dim=1) + 1
+        pred_mask = pred_mask.squeeze().cpu()
+
+        mask = mask.squeeze()
+        img_np  = np.transpose(image.numpy(), (1, 2, 0))
+        fig, arr = plt.subplots(1, 3, figsize=(15, 15))
+        arr[0].imshow(img_np)
+        arr[0].set_title('Image ')
+        arr[1].imshow(mask)
+        arr[1].set_title('Actual Mask')
+        arr[2].imshow(pred_mask)
+        arr[2].set_title('Predicted Mask')    
